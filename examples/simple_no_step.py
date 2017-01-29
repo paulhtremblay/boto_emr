67765c3819bb0d95dd27e813a118744d0ad70f3b
Paul Tremblay <paulhtremblay@gmail.com>
2017-01-29 14:28:54 -0800

    adding simple example

---
+++ examples/simple_no_step.py
@@ -0,0 +1,24 @@
+﻿"""
+Creates an EMR cluser with one master node and no slaves.
+The cluster will terminate as soon it is spun up.
+
+"""
+import boto3
+session = boto3.Session(profile_name='admin')
+client = session.client('emr')
+response = client.run_job_flow(
+   Name = "tutorial example 1",
+   ReleaseLabel = 'emr-5.2.1',
+   Instances = {
+       'InstanceGroups': [{
+           'InstanceRole': 'MASTER',
+           'Market': 'SPOT',
+           'BidPrice': '.6',
+           'InstanceType': 'm1.medium',
+           'InstanceCount': 1
+       }],
+   },
+   JobFlowRole = 'EMR_EC2_DefaultRole',
+   ServiceRole = 'EMR_DefaultRole',
+)
+print(response)
