2791d4a3e9f86579f56f5d5dbf2e2ec896ad9b68
Paul Tremblay <paulhtremblay@gmail.com>
2017-02-09 14:07:40 -0800

    needed for crawl paths

---
+++ boto_emr/get_s3_common_crawl_paths.py
@@ -0,0 +1,29 @@
+﻿import boto3
+import os
+
+
+def list_objects(bucket_name, prefix):
+    final = []
+    session = boto3.Session(profile_name = 'admin')
+    client = session.client('s3')
+    next_token = 'init'
+    kwargs = {}
+    while next_token:
+        if next_token != 'init':
+            kwargs.update({'ContinuationToken': next_token})
+        response  = client.list_objects_v2(Bucket=bucket_name, Prefix = prefix,
+                **kwargs)
+        next_token = response.get('NextContinuationToken')
+        for contents in response.get('Contents', []):
+            key = contents['Key']
+            yield key
+
+def get_segments(bucket_name = 'commoncrawl', prefix =
+        'crawl-data/CC-MAIN-2016-50/segments'):
+    return list_objects(bucket_name = bucket_name, prefix = prefix)
+
+def get_crawl_paths(the_type, recursion_limit = None):
+    assert the_type in ['crawldiagnostics']
+    return list(filter(lambda x: os.path.split(os.path.split(x)[0])[1] == the_type,
+            get_segments()))
+
---
+++ boto_emr/tests/test_get_s3_crawl_paths.py
@@ -0,0 +1,30 @@
+﻿import unittest
+import boto_emr.get_s3_common_crawl_paths as crawl_paths
+import pprint
+pp = pprint.PrettyPrinter(indent = 4)
+
+class TestGetS3CrawlPaths(unittest.TestCase):
+
+    @unittest.skip("time")
+    def test_list_objects_returns(self):
+        l = crawl_paths.list_objects(bucket_name = 'paulhtremblay', prefix = 'test/')
+        self.assertTrue(len(l) > 0)
+
+    @unittest.skip("time")
+    def test_list_objects_returns_empty_list_for_bad_prefix(self):
+        l = crawl_paths.list_objects(bucket_name = 'paulhtremblay', prefix = '1ebc325')
+        self.assertTrue(len(l) == 0)
+
+    @unittest.skip("time")
+    def test_get_segments_returns_list_greater_than_0(self):
+        l = crawl_paths.get_segments()
+        self.assertTrue(len(l) > 0)
+
+    def test_get_crawl_paths_returns_list_greater_than_0(self):
+        l = crawl_paths.get_crawl_paths(the_type = 'crawldiagnostics',
+                recursion_limit = 10)
+        pp.pprint(l)
+
+
+if __name__ == '__main__':
+    unittest.main()
